525d63abbc1f3dfa76b534e5b5f940615ba152b3
kubofac <kubofac@gmail.com>
2025-11-08 11:02:29 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,55 @@
+const CACHE_NAME = 'KLAP_messenger_ver3';
+const urlsToCache = [
+  '/',
+  '/index.html',
+    '/manifest.json',
+  '/service-worker.js',
+  '/icon192.png',
+  '/icon512.png',
+  // 画像やJavaScriptファイルなど、アプリを構成するすべての静的ファイルをここに追加
+  
+  
+  // その他のCSSやJSファイルなど
+];
+
+// インストールイベント: キャッシュにファイルを保存
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// フェッチイベント: キャッシュからリソースを返す
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // キャッシュにリソースがあればそれを返す
+        if (response) {
+          return response;
+        }
+        // なければネットワークから取得する
+        return fetch(event.request);
+      })
+  );
+});
+
+// アクティベートイベント: 古いキャッシュを削除
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
